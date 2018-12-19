4e1a4fc99869470d3b5b0473fac2e28f73f7b1e1
zhangtao <ztaobuct@163.com>
2018-12-19 16:56:29 +0800

    creat project

--- src/helloworld.java
+++ src/helloworld.java
@@ -1,6 +1,9 @@
 public class helloworld {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        System.out.println("Hello Worl");
+
+        System.out.println("Hello Worl");
+        System.out.println("Hello Worl");
 
     }
 }
