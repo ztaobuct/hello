9d59a1876d53362559b55919e0b98263992a2a4c
zhangtao <ztaobuct@163.com>
2018-12-19 16:58:18 +0800

    adn

--- src/helloworld.java
+++ src/helloworld.java
@@ -2,8 +2,6 @@ public class helloworld {
     public static void main(String[] args){
         System.out.println("Hello Worl");
 
-        System.out.println("Hello Worl");
-        System.out.println("Hello Worl");
 
     }
 }
